5e82109ee81fcbeeaa0ad9dbe753c1c74bffb556
Shamaiel <mswani258@gmail.com>
2023-11-29 10:30:05 +0530

    added home component

---
+++ Frontend/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      Cards sec
+    </div>
+  )
+}
+
+export default Card
+
---
+++ Frontend/src/page/CreatePost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePost = () => {
+  return (
+    <div>
+      Creat Post
+    </div>
+  )
+}
+
+export default CreatePost
